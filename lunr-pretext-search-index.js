87e6c5cb38d1e05cc1a9a6dc2c16da91efc5198b
111F-Syllabus <budboman@gmail.com>
2023-11-15 17:29:14 -0500

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1,15 +1,6 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "Math111F-2",
-  "level": "1",
-  "url": "Math111F-2.html",
-  "type": "Chapter",
-  "number": "1",
-  "title": "Test Section",
-  "body": " Test Section  "
-},
-{
   "id": "Class_Meeting_Information-2",
   "level": "1",
   "url": "Class_Meeting_Information-2.html",
@@ -28,90 +19,90 @@ var ptx_lunr_docs = [
   "body": " Adjustments Due to Illness          If you are unable to attend class due to illness, let me know as soon as you are able to do so. I will be reasonable as long as you keep me informed.  "
 },
 {
-  "id": "Math111F-4",
+  "id": "Math111F-3",
   "level": "1",
-  "url": "Math111F-4.html",
+  "url": "Math111F-3.html",
   "type": "Section",
   "number": "2",
   "title": "How to Succeed in this Course",
   "body": "How to Succeed in this Course  Because you learn math by doing math your grade in this course will depend on  Your success at solving problems take from the textbook(s) the practice problems, the exams, the quizzes, and the Webwork and  Your diligence in trying to solve those problems.    So the recipe for success is simple: Work through and solve as many problems, from as many sources as you possibly can. You must do this yourself. No one can do it for you. My role is to help you think clearly about how to approach a problem, how solve it, and how to know that you have solved it.  I will begin every class session by asking if there are any questions. This is your opportunity to learn. If you have been working unsuccessfully on a problem, or if you have solved a problem but are still not sure that you understand it, ask about it in class and we will all discuss it then.  I do not guarantee that I will solve the problem for you (although sometimes I might). That is not my role. My role is to help you understand the mathematics you are here to learn. Most often I will simply give you a push in the right direction, tell you to try again, and suggest that you ask again if you still can't make any progress.  "
 },
 {
-  "id": "Math111F-5",
+  "id": "Math111F-4",
   "level": "1",
-  "url": "Math111F-5.html",
+  "url": "Math111F-4.html",
   "type": "Section",
   "number": "3",
   "title": "Class Participation",
   "body": "Class Participation  Your level of participation during class will be graded and this will be the single largest individual piece of your final course grade. Specifically, you start every class day with 2 points. If you fail to participate in the day's work you can lose one or both points, subject to my discretion. More detail on this follows.  I rarely lecture for the entirety of a class. Instead most of any given class period will be spent with you working on a problem, or a set of problems at your desks. If you are not working on the problems it will cost you at least one participation point. Sometimes I will call on a class member at random to talk with me about a problem. If you refuse to respond it will cost you at least one participation point.  But be clear about this. These are participation points, not attendance points and not getting-the-right-answer points.  It is perfectly acceptable to be unable to solve a problem or answer a question in class. It is not acceptable to refuse to discuss the problem. For example, suppose call on you to share your solution to some problem, but you haven't been able to finish it. It is perfectly acceptable to say, \"I wasn't able to finish.\" In that case I'll ask you to explain whatever progress you have made and the discussion will continue. It is not acceptable to say, \"I don't know. Call on someone else.\" and then refuse to discuss the problem further. You will lose your participation points for that day. I do not expect you will able to correctly answer any random question that I throw at you. But I do expect you to participate in the classroom discussions.  I cannot emphasize this enough. Solving the problem is far less important trying to solve the problem, for it is in the trying that learning happens. Being able to solve a given problem means that you have learned as much as you can from it, and it is time to move on to a problem you don't know how to solve.  You will only succeed if you spend as much time trying to solve problems as you possibly can. I will therefore make every effort to keep the lecture time to a bare minimum so that you can work on problems in class as much as possible.  Teaching others is also a good way to refine and deepen your understanding. A secondary goal for having you solve problems in class rather than on your own is so that you can learn from each other as well as from me. If I throw out a problem that you find easy, look around for a classmate who is struggling and offer to help them out. You will learn as much as the student you are helping. Who knows, you might even make a friend.  Class participation will contribute 450 points toward your final grade. The lowest two will be dropped.  "
 },
 {
-  "id": "Math111F-6",
+  "id": "Math111F-5",
   "level": "1",
-  "url": "Math111F-6.html",
+  "url": "Math111F-5.html",
   "type": "Section",
   "number": "4",
   "title": "WeBWorK",
   "body": "WeBWorK  I will also assign regular homework problems via the WeBWork system. Each WebWork assignment will only be available to you for a fixed amount of time, so get started early and keep trying.  You can earn as many as points toward your final course grade from the WeBWorK problems. You can keep submitting solutions to any WeBWork problem until you either get it right or the due date passes. All that will be recorded is whether or not you finally did get the right answer before the assignment closes.  The first assignment (named 'Orientation') will not count toward your grade, but you should do it anyway. It is essentially a short tutorial on how to use WeBWorK.  It is extremely unwise to wait until the night a set of WeBWorK problems is due to begin doing them. This is because it will not be clear how to do some of the problems the first time you look at them. If you don't start on them until the night they are due you will not have a chance to ask about them in class, so you will not learn what you need to learn from those problems.  WeBWorK advice  Your best option is to open and try the WeBWorK problems as soon as they open. Some of them will be challenging and you don't want to be doing them in a panic at the last minute.  This will be reflected in your exam grades and your final course grade.  The WeBWorK will contribute up to points to your course grade.  "
 },
 {
-  "id": "Math111F-7-2",
+  "id": "Math111F-6-2",
   "level": "1",
-  "url": "Math111F-7-2.html",
+  "url": "Math111F-6-2.html",
   "type": "Subsection",
   "number": "5.1",
   "title": "Exams",
   "body": "Exams  There will be exams given during the semester; one near the middle of the semester and the second at the end of the semester during Final Exams week.   Exam Dates  We will decide the date of the mid-semester exam later. We will make this decision together.   Each exam will be on the material covered since the last exam, and will contribute points toward your course grade. The exam problems will most likely be taken from the Practice Problems, the textbook, or the WeBWorK but this is not guaranteed.  You can make up a missed exam during the first week after it was given for a score reduction of %, and during the second week after it was given for a score reduction of %. After that your score on that exam will be . The exams will contribute up to points toward your grade in the course.  "
 },
 {
-  "id": "Math111F-7-3",
+  "id": "Math111F-6-3",
   "level": "1",
-  "url": "Math111F-7-3.html",
+  "url": "Math111F-6-3.html",
   "type": "Subsection",
   "number": "5.2",
   "title": "Quizzes",
   "body": "Quizzes   Week 1  The first week is a bit problematic since we only meet twice a week. We'll discuss how we will handle the first quiz on the first day of class.  There will be a quiz at the end of class every Wednesday, except when exams are scheduled. Each quiz will be:   minutes long,  worth (quiz) points.  The average of all of the quizzes will contribute 150 (course) points toward your final course grade. There will be no makeup quizzes for any reason because the lowest quiz score will be dropped.  "
 },
 {
-  "id": "Math111F-8",
+  "id": "Math111F-7",
   "level": "1",
-  "url": "Math111F-8.html",
+  "url": "Math111F-7.html",
   "type": "Section",
   "number": "6",
   "title": "Computing Technology",
   "body": "Computing Technology  Computing technology is a reality of the world we live in. You have been using it all of your life and you will probably continue to do so. You are welcome to use whatever computing technology you want to during class, during exams, and during quizzes.  But this course is not about how to use computing technology so your best strategy is to use it in a manner which helps you learn. For example, use technology to confirm your problem solutions after you've made a serious effort to solve them, not before.  As I said, you are welcome to use any computing tech you want during quizzes and exams, but I recommend very strongly against it.  I repeat: I recommend against using computing technology during quizzes and\/or exams, but I will allow you to do it.  This is not me being kind. Quite the opposite.  Because this course is about understanding and applying abstract concepts I design the quiz\/exam problems to test your understanding not your ability to compute. Such problems will always require you to explain the results of any computations that you do, and the explanation is much more important than the computation because that is where your level of understanding will be displayed.  For example, a quiz or exam problem on the Quadratic Formula might be something like this.    Problem: Show that the solutions of the equation are and .   An excellent response to this problem would be   Since we have Thus the solutions of are the same as the solutions of or , which are and .   A mediocre response to this problem would be       A poor response to this problem would be       In fact this is such a poor response that it would get a score of zero. If you don't see why please ask me in class. I'm sure you are not the only one.  "
 },
 {
-  "id": "Math111F-9",
+  "id": "Math111F-8",
   "level": "1",
-  "url": "Math111F-9.html",
+  "url": "Math111F-8.html",
   "type": "Section",
   "number": "7",
   "title": "Grading Policies",
   "body": "Grading Policies  You can earn as many as 1000 points during this semester:  450 for Participation  300 from the WeBWork problems,  150 from the quizzes,  100 from the two exams (50 points each).    The grade you receive at the end of this semester will be computed as follows:  Let be the number of Participation points you earned, and let be the number of days class is conducted. Then your participation score will be .  Participation Score  For example: Suppose this class is conducted times. Then . If you earned points (this is the most you can earn) in of them, and only in the other two then .  In that case your participation score is .   Let be the average of your quiz scores at the end of the semester. Then your quiz score is .   Quiz Scores  For example, if quizzes are given on which you got , , and points then .  In that case your quiz score is .   Let be the number of Webwork problems you solved and let be the total number of Webwork problems available. Then your Webwork score is .  Finally let and be your score on each of the exams.  Please Check This Formula!!  Please make sure this formula is consistent with the information above and let me know if it is not . Some semesters I tweak the grading scheme a bit and inconsistencies can crop up. I try to catch these before the semester begins but sometimes I miss stuff.   Your total points =    I do not use the gradebook on Moodle. If something shows up there it is because I couldn't find a way to prevent it. I keep grade records offline in a spreadsheet. Moreover, I do not keep your grade records in a form that allows me to tell you what your \"current\" grade is. If you want a running record of your \"current\" grade you have to keep your own records of your Quiz, WeBWorK, Participation, and Exam scores.     Pluses and Minuses:  At the end of the semester if your computed grade seems to me to be lower than your level of mastery warrants I reserve the right to raise your grade with a plus or minus.  For example, if you have points your computed grade would be a C but it is at the very high end of the C range. If, in my opinion, your level of mastery indicates that a higher grade is appropriate I will raise this to a B-.  In similar circumstances if you have points I would raise your grade to a C+. Such adjustments will be rare and will be done solely at my discretion.  Finally, I will never lower your grade in this manner. If your score is points you will get no less than a C.  Keep in mind that grades are earned not given.    Your course grade will be based on your point total, according to the following table.  Grading Scale          Grade  Range    A  900-1000    B  800-899    C  700-799    D  600-699    F        "
 },
 {
-  "id": "Math111F-9-5",
+  "id": "Math111F-8-5",
   "level": "2",
-  "url": "Math111F-9.html#Math111F-9-5",
+  "url": "Math111F-8.html#Math111F-8-5",
   "type": "Example",
   "number": "7.0.1",
   "title": "Participation Score.",
   "body": "Participation Score  For example: Suppose this class is conducted times. Then . If you earned points (this is the most you can earn) in of them, and only in the other two then .  In that case your participation score is .  "
 },
 {
-  "id": "Math111F-9-7",
+  "id": "Math111F-8-7",
   "level": "2",
-  "url": "Math111F-9.html#Math111F-9-7",
+  "url": "Math111F-8.html#Math111F-8-7",
   "type": "Example",
   "number": "7.0.2",
   "title": "Quiz Scores.",
   "body": " Quiz Scores  For example, if quizzes are given on which you got , , and points then .  In that case your quiz score is .  "
 },
 {
-  "id": "Math111F-9-13",
+  "id": "Math111F-8-13",
   "level": "2",
-  "url": "Math111F-9.html#Math111F-9-13",
+  "url": "Math111F-8.html#Math111F-8-13",
   "type": "Note",
   "number": "7.0.3",
   "title": "Pluses and Minuses:.",
@@ -120,7 +111,7 @@ var ptx_lunr_docs = [
 {
   "id": "GradeScale",
   "level": "2",
-  "url": "Math111F-9.html#GradeScale",
+  "url": "Math111F-8.html#GradeScale",
   "type": "Table",
   "number": "7.0.4",
   "title": "Grading Scale",
@@ -136,9 +127,9 @@ var ptx_lunr_docs = [
   "body": " Contacting Me  You may contact me by any of the following methods (listed in decreasing order of effectiveness):    Speak up during class  I will be in class with you days a week. This is your time. You have my undivided attention. If you have a question which is not personal this is the best time to get an answer.   Speak to me immediately before or after class  Some questions might involve personal information that you don't want to share with the rest of the class. Simple matters can be handled quickly before or after class. If the issue is more complex we can arrange to meet another time.  If you are not free immediately before or after class but need to speak to me let me know and we'll work out a mutually convenient time to speak.                                      Email  My email address is eboman@gettysburg.edu . I usually check it several times per day. Feel free to use it.  However, I do not sit at my desk and continuously monitor my email. Sometimes it will take time for me to respond. Usually you will get a response by the next day at the latest, expecially if I don't see your email until late in the day.    If I Don't Respond to an Email       If you send me an email and I do not respond it does not mean that I hate you, or that I'm ignoring you. It simply means that I didn't see it.  If I see your email I will make every possible effort to respond. If I don't respond in a reasonable time please send it again. Or, if there is no rush, bring your the issue to me during the next class. I'd rather talk to you in person anyway.        "
 },
 {
-  "id": "Math111F-11",
+  "id": "Math111F-10",
   "level": "1",
-  "url": "Math111F-11.html",
+  "url": "Math111F-10.html",
   "type": "Section",
   "number": "9",
   "title": "Semester Schedule",
@@ -147,16 +138,16 @@ var ptx_lunr_docs = [
 {
   "id": "SemesterSchedule",
   "level": "2",
-  "url": "Math111F-11.html#SemesterSchedule",
+  "url": "Math111F-10.html#SemesterSchedule",
   "type": "Table",
   "number": "9.0.1",
   "title": "Semester Schedule",
   "body": " Semester Schedule          By this date      you should have read these sections.      Monday    Come to class prepared to discuss and\/or solve these problems.      Wednesday    Come to class prepared to discuss and\/or solve these problems.       8\/28\/23    the Preface, Chapter 1, Sections 2.1, 2.2, 2.3, and 2.4      #9, #12, #15    #19, #20     9\/4\/23    Sections 2.6 and 2.7  Chapter 3    #38, #47, #52    #53, #57     9\/11\/23    Chapter 4    #89, #90, #94, #95    #104, #122, #123, #134     9\/18\/23    Chapter 5    #158, #162, #163, #166a,b    #168a(ii,iii), #174, #177a,c,d, #198     9\/25\/23    Chapter 6    #232a, b, #240    #257, #258     10\/2\/23    Sections 7.1, 7.2, 7.3, 7.4    #263, #266, #273, #286    #288, #293, #294     10\/9\/23    Sections 7.5, 7.6, 8.1, 8.2    #326, #328, #333, #336    #346, #348, #352     10\/16\/23    Sections 8.3, 8.4, 8.5, 8.6, 8.7    #356, #371, #374, #375, #376    #380, #394, #396, #399, #400     10\/23\/23    Chapter 9    #418m, #420(b,f), #421(a,b,f,g)    #422(a,d), #425(a,c,e)     10\/30\/23    Chapter 10    #428, #429, #430, #434(a,b)    #435, #436, #437     11\/6\/23    Chapter 11    #446, #448, #452(a,c,g)    #456, #472(a,e,h), #473(a,b,c)     11\/13\/23    TBD             11\/20\/23      TBD         Note: No class on 11\/21\/23 due to Thanksgiving holiday.     11\/27\/23    Chapter 12, Sections 13.1 through 13.6    #480i, #481(a,e), #486(a,c), #487    #491, #492, #505, #575, #518     12\/4\/23       Chapters 14 and 15    #521, #523, #525, #527         "
 },
 {
-  "id": "Math111F-12",
+  "id": "Math111F-11",
   "level": "1",
-  "url": "Math111F-12.html",
+  "url": "Math111F-11.html",
   "type": "Section",
   "number": "10",
   "title": "Videos",
